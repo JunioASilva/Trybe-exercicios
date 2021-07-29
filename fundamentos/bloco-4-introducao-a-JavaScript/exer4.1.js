7b0bda036cdc7dac9a839ff5555a2a1cf9142def
JunioASilva <junioalves2005@hotmail.com>
2021-07-29 15:10:47 -0300

    exercicios dia 4.1

---
+++ fundamentos/bloco-4-introducao-a-JavaScript/exer4.1.js
@@ -0,0 +1,109 @@
+// let number1 = 20;
+// let number2 = 25;
+// let resultado =0;
+// let number3 = 28;
+
+// resultado = number1 + number2;
+
+// resultado = number1 - number2;
+
+// resultado = number1 * number2;
+
+// resultado = number1 / number2;
+
+// resultado = number1 % number2;
+
+// console.log(resultado);
+
+// if(number1 > number2){
+//     console.log("sim é maior");
+// }
+// else{
+//    console.log("Não é menor");
+// }
+
+// if(number1 > number2 && number1 > number3){;
+//     console.log("o numero maior é o number1");
+//     }   
+//     else if(number2 > number1 && number2 > number3){
+//         console.log("o numero maior é o number2");
+//     }
+//     else {
+//         console.log("o maior é o number3");
+//     }
+
+    // let parametro = 0;
+
+    // let valor = Math.sign(parametro);
+
+    // if(valor == 1){
+    //     console.log("Positive");
+    // }
+    // else if(valor == -1){
+    //     console.log("negativo");
+    // }
+    // else{
+    //     console.log("zero");
+    // }
+
+    // let angulo1 = 100;
+    // let angulo2 = 15;
+    // let angulo3 = 65;
+    // let somaAngulos = angulo1 + angulo2 + angulo3;
+
+    // if(somaAngulos == 180){
+    //     console.log(true);
+    // }
+    // else{
+        // console.log(false);
+    // }
+    // else {
+    //     console.log("error")
+    // }
+ 
+    // let peçaDeXadrez = "bispo";
+
+    // switch(peçaDeXadrez.toLowerCase()){
+    //     case "rei":
+    //         console.log("anda apenas uma casa para qualquer lado");
+    //         break;
+    
+    //     case "cavalo":
+    //         console.log("anda em L");
+    //         break;
+    //     case "bispo":
+    //         console.log("anda na diagonal");
+    //         break;
+    //     case "torre":
+    //         console.log("anda em linha reta"); 
+    //         break;
+    //     case "peão":
+    //         console.log("anda em linha reta");
+    //         break;
+    //     case "rainha":
+    //         console.log("anda para qualquer casa");               
+    // }      
+        let grade = 101;
+
+        if(grade <= 0 || grade > 100){
+            console.log("Error");
+        }
+        else if(grade >= 90){
+            console.log("A");
+        }
+        else if (grade >= 80){
+            console.log("B");
+        }
+        else if (grade >= 70){
+            console.log("C");
+        }
+        else if (grade >= 60){
+            console.log("D");
+        }
+        else if (grade >= 50){
+            console.log("E");
+        }
+        else if (grade > 50){
+            console.log("F");
+        }
+        
