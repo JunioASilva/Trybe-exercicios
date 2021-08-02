efb5f099bf8747989a6bfde129a102aeafa5de98
JunioASilva <junioalves2005@hotmail.com>
2021-08-02 13:34:18 -0300

    exercicios dia 4.2

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/scripts.js
@@ -0,0 +1,37 @@
+// let numero = 7;
+
+// for(let contador = 1; contador <= 9; contador ++){
+//     console.log(contador * numero);
+// }
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for(let index = 0; index < groceryList.length; index ++){
+//     console.log(groceryList[index])
+// }
+
+    // let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+    //     for(let name of names){
+    //         console.log(name)
+        
+    // }
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    let soma = 0;
+    let divisor = numbers.length;
+       
+       
+       for(let i = 0; i < divisor; i ++){
+            soma += numbers[i];
+            
+       }
+       let resultado = soma/divisor;
+            console.log(resultado)
+       
+         
+        
+        
+    
+    
+        
+
+    
