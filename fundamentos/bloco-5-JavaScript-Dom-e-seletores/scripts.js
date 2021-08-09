9e19cd24cd1f670e155a306194d734ecf2a7baa0
JunioASilva <junioalves2005@hotmail.com>
2021-08-09 16:37:15 -0300

    Iniciando exercicios 5.1

---
+++ fundamentos/bloco-5-JavaScript-Dom-e-seletores/scripts.js
@@ -0,0 +1,8 @@
+ const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.style.color = "blue";
+secondParagraph.style.backgroundColor = "red"
+
+document.getElementById("page").innerText = "Harry Potter";
