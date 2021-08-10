c3ef381afa9cc7d6268f463b8eaba7e7beb1dd4a
JunioASilva <junioalves2005@hotmail.com>
2021-08-10 13:31:31 -0300

    finalizando exercicios 5.1

--- fundamentos/bloco-5-JavaScript-Dom-e-seletores/scripts.js
+++ fundamentos/bloco-5-JavaScript-Dom-e-seletores/scripts.js
@@ -1,8 +1,22 @@
- const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
+// document.getElementsByTagName("p")[0].innerText = "Empregado e Feliz";
 
-const secondParagraph = document.getElementById("second-paragraph");
-secondParagraph.style.color = "blue";
-secondParagraph.style.backgroundColor = "red"
+// document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
 
-document.getElementById("page").innerText = "Harry Potter";
+// document.getElementsByClassName("main-content")[1].style.backgroundColor = "white";
+
+// document.getElementsByTagName("h1")[0].innerHTML = "Exercicios 5.1 - JavaScript";
+
+function upperCaseParagraph (){
+let paragraph = document.getElementsByTagName("p");
+paragraph.innerHTML = paragraph.innerHTML.toUpperCase();
+}
+upperCaseParagraph();
+
+function showParagraphs() {
+    let paragraphs = document.getElementsByTagName("p");
+    for (let index = 0; index < paragraphs.length; index += 1) {
+      console.log(paragraphs[index].innerHTML);
+    }
+  }
+  showParagraphs();
+
